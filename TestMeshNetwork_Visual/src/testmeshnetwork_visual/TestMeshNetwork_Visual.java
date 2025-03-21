2d9aebc33fd95db66cff47a0d01bc97bc2ce6e1d
kenttXVI <157788958+kenttXVI@users.noreply.github.com>
2025-03-22 00:27:23 +0100

    Add files via upload

---
+++ TestMeshNetwork_Visual/src/testmeshnetwork_visual/TestMeshNetwork_Visual.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package testmeshnetwork_visual;
+
+/**
+ *
+ * @author NET-0011
+ */
+public class TestMeshNetwork_Visual {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Frame_Test f = new Frame_Test();
+        f.setVisible(true);
+    }
+    
+}
